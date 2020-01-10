dc9dd48cea89f2b19400682660b3e0d50c2dd4b7
Diego29carlos <juan29-diego@hotmail.com>
2020-01-10 17:42:59 -0300

    agregue estilo

--- 03 Sentando las bases/01 querySelector/scripts/main.js
+++ 03 Sentando las bases/01 querySelector/scripts/main.js
@@ -1,1 +1,8 @@
-console.info('Cuando veas éste mensaje en consola, ya habrás terminado ésta parte del ejercicio. Borra ésta línea cuando ya la hayas visto alguna vez en la consola al acceder a ésta página.')
+const favorito =document.querySelector("comment");
+
+for(let i = 0; i<favorito.length;i++){
+
+    if(i=favorito.length-2){
+        favorito[i].classList.add("favorito");
+    }
+}
