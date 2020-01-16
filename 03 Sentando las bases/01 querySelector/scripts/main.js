750a39153caa1797927f8bf9319cb11d2bd7e0f9
Diego29carlos <juan29-diego@hotmail.com>
2020-01-16 16:16:06 -0300

    trabajo terminado

--- 03 Sentando las bases/01 querySelector/scripts/main.js
+++ 03 Sentando las bases/01 querySelector/scripts/main.js
@@ -1,8 +1,55 @@
-const favorito =document.querySelector("comment");
+/*
+function favorito (comentario)
 
-for(let i = 0; i<favorito.length;i++){
+for(let i = 0; i<favorito.length;i++) {
 
-    if(i=favorito.length-2){
-        favorito[i].classList.add("favorito");
+    if(favorito[i].classList.add("favorito");
+
+    {
+    }
+}*/
+
+const comentarios = document.querySelectorAll(".comment--text");
+
+
+for(let i = 0; i<comentarios.length; i++){
+    if(comentarios[i] === comentarios[1]){
+        comentarios[i].classList.add("commentjeny")
+    }
+        
+}
+
+const caja2 = document.querySelectorAll("hr");
+   
+  for(let i= 0; i< caja2.length; i++){
+      caja2[i].classList.add("cajahr")
+  }
+
+
+
+
+  for(let i= 0; i<comentarios.length; i++){
+      if(comentarios[i].innerText.length > 50){
+        comentarios[i].classList.add("mayor");
+      }
+
+  }
+
+
+for(let i= 0; i< comentarios.length; i++){
+    if (comentarios[i] === comentarios[0]){
+        comentarios[i].classList.add("commetlargo")
     }
 }
+
+
+/*
+array.length : cantidad de bloques que tiene
+
+array[n]: lo que tiene cada bloque n
+
+array[n].innerText: te devuelve lo que tiene dentro
+
+
+*/ 
+
--- README.md
+++ README.md
@@ -2,3 +2,27 @@
 ## Ejercicios de Repaso - JavaScript
 
 Repositorio para ejercicios de repaso de JavaScript
+
+
+
+evento igual a la interaccion con el usuario y lo escucha el navegador 
+callback se ejecuta dentro de una funcion
+
+.bla clase 
+#bla id
+bla etiqueta 
+
+
+ seleccionar el comentario favorito de los 3 que escribi
+  
+ el comentario de jeny fue el que mas me gusto 
+
+ marcarlo con un color o fondo 
+
+ pasarlo a codigo 
+
+ 
+
+ 
+
+
